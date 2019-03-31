2335a1a9607b584ba3f8e863bda95b4aebd440cb
Boris Mizhen <boris@boriska.com>
2019-03-31 14:55:37 -0400

    specfied python3

--- setup.py
+++ setup.py
@@ -5,22 +5,23 @@ import pathlib
 HERE = pathlib.Path(__file__).parent
 
 # The text of the README file
-README = (HERE / "README.md").read_text()
+README = (HERE / 'README.md').read_text()
 
 setup(
-    name="rec-avro",
-    version="0.0.1",
-    description="Avro schema and data converters supporting storing arbitrary nested python data structures.",
+    name='rec-avro',
+    version='0.0.1',
+    description='Avro schema and data converters supporting storing arbitrary nested python data structures.',
     long_description=README,
-    long_description_content_type="text/markdown",
-    url="https://github.com/bmizhen/rec-avro",
-    author="Boris Mizhen",
-    author_email="rec-avro@boriska.com",
-    license="MIT",
+    long_description_content_type='text/markdown',
+    url='https://github.com/bmizhen/rec-avro',
+    author='Boris Mizhen',
+    author_email='rec-avro@boriska.com',
+    python_requires='>=3',
+    license='MIT',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     install_requires=[],
-    setup_requires=["pytest-runner"],
+    setup_requires=['pytest-runner'],
     tests_require=['fastavro', 'pytest']
 )
 
